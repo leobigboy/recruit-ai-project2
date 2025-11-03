8274d69dd49780c3faa09d9ec883bbc8d012c5e0
Vo Huynh Thai Bao <94208129+vina123baov@users.noreply.github.com>
2025-11-03 09:17:08 +0700

    AIchatbot

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 // src/components/layout/MainLayout.tsx
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
+import FloatingChatbot from "@/components/FloatingChatbot";
 
 export function MainLayout() {
   return (
@@ -9,6 +10,9 @@ export function MainLayout() {
       <main className="flex-1 ml-64 relative z-0">
         <Outlet />
       </main>
+      
+      {/* Floating Chatbot - Sẽ xuất hiện ở tất cả các trang */}
+      <FloatingChatbot />
     </div>
   );
 }
